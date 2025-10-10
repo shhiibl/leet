1bb83d12a43fa3229fcfbf660b0339ad84cc41ce
shhiibl <muhammedshibalmp1@gmail.com>
2025-10-10 14:11:48 +0530

    Time: 12 ms (21.38%) | Memory: 61.8 MB (67.80%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+     const squaredNums = nums.map(num => num * num);
+
+        return squaredNums.sort((a, b) => a - b);
+};
+
+
+
+
+
